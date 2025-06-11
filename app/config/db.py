7d4bd87c2ae98f49c2991280f2cc393f85b2012c
MaqboolAhmad01 <maqbool.riazahmad@gmail.com>
2025-06-11 07:38:57 +0500

    Set up alembic and create user table

--- app/config/db.py
+++ app/config/db.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-POSTGRES_DATABASE_URL = 'postgresql://postgres:Bright#1270@localhost/fastapi'
+POSTGRES_DATABASE_URL = os.getenv("POSTGRES_CONN_STRING")
 
 engine = create_engine(POSTGRES_DATABASE_URL)
 
